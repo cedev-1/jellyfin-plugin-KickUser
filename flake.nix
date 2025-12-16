1cb1a34ced945995e4d1e01d16e6559566a2b432
cedev-1 <121963179+cedev-1@users.noreply.github.com>
2025-12-16 19:28:12 +0100

    init nix env

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Jellyfin plugin Discord Notifier";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs { 
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # dotnetCorePackages.sdk_8_0-bin
+            dotnetCorePackages.sdk_9_0-bin
+            
+            go-task
+
+          ];
+        };
+      });
+}
